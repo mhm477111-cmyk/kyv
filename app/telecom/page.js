8ce8a8a407e980099c281f669687ab10b6f29a47
mhm477111-cmyk <mhm477111@gmail.com>
2026-04-25 14:35:02 +0300

    Update page.js

--- app/telecom/page.js
+++ app/telecom/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { db } from '@/lib/firebaseConfig';
+import { db } from '@/lib/firebaseConfigV2';
 import { collection, onSnapshot, doc, updateDoc, addDoc, deleteDoc } from 'firebase/firestore';
 
 export default function TelecomSystem() {
